5bd5bb9f692b0ada432f9dc05bada8e090fc9458
zman <hxfdarling@hotmail.com>
2018-05-02 00:07:46 +0800

    feat: update

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  plugins: ['prettier'],
+  extends: ['@webpack-contrib/eslint-config-webpack'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { singleQuote: true, trailingComma: 'es5', arrowParens: 'always' },
+    ],
+  },
+};
